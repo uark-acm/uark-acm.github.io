a56cbcd2112e57db106f0d1654de6c7987202d0b
AidanM95 <146774253+AidanM95@users.noreply.github.com>
2024-02-08 12:57:24 -0600

    Footer for ACM website

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import acmLogo from '@/public/img/acm-logo.png';
+import Footer from './components/footer/index';
 
 const Home: NextPage = () => {
     return <>
         <div className={'flex items-center flex-col m-5'}>
             <h1 className={'text-4xl mb-[100px]'}>The UARK ACM website is under development :)!</h1>
             <Image src={acmLogo} alt='ACM Logo' width={500} height={500} />
+            <Footer />
         </div>
     </>
 }
