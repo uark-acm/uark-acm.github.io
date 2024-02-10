46b241aa8fdc4e7c5097085c20636ee9504d6f59
Alex Prosser <apsoutherngaming07@gmail.com>
2024-02-10 17:22:34 -0600

    some edits

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+/**
+ * components/footer/index.tsx
+ * 
+ * Author: Alex Prosser, Jordi Castro
+ * Date: 02/08/2024
+ */
+
 import { NextPage } from 'next';
 import Image from 'next/image';
+import Footer from '@/components/footer';
 import acmLogo from '@/public/img/acm-logo.png';
 import kirby from '@/public/img/kirby.png';
-import Footer from './components/footer/index';
 
 const Home: NextPage = () => {
 
@@ -14,28 +21,28 @@ const Home: NextPage = () => {
     return (
         <div className={'flex items-center flex-col m-5'}>
             <h1 className={'text-4xl mb-[100px]'}>The UARK ACM website is under development :)!</h1>
-            <div className="carousel">
+            <div className={'carousel'}>
                 {/* carousel description */}
-                <div className='carousel-description'>
-                <h2 className="article-title">Article Title</h2>
-                <h3 className="author">by Author</h3>
-                <p className="article-description">Article description that will be cut off to save space...</p>
+                <div className={'carousel-description'}>
+                    <h2 className={'article-title'}>Article Title</h2>
+                    <h3 className={'author'}>by Author</h3>
+                    <p className={'article-description'}>Article description that will be cut off to save space...</p>
                 </div>
                 {/* temp image*/}
-                <div className='carousel-image'>
-                    <Image src={kirby} alt='temp image' width={500} height={100} ></Image>
+                <div className={'carousel-image'}>
+                    <Image src={kirby} alt={'Temporary Image'} width={500} height={100} ></Image>
                 </div>
-                <div className='carousel-pagination'>
-                    <span className='pagination-circle'>&#x25CF;</span>
-                    <span className='pagination-circle'>&#x25CF;</span>
-                    <span className='pagination-circle'>&#x25CF;</span>
+                <div className={'carousel-pagination'}>
+                    <span className={'pagination-circle'}>&#x25CF;</span>
+                    <span className={'pagination-circle'}>&#x25CF;</span>
+                    <span className={'pagination-circle'}>&#x25CF;</span>
                 </div>
                 {/* left arrow*/}
-                <div className='arrow left-arrow' >&#9664;</div>
+                <div className={'arrow left-arrow'}>&#9664;</div>
                 {/* right arrow*/}
-                <div className='arrow right-arrow'>&#9654;</div>
+                <div className={'arrow right-arrow'}>&#9654;</div>
             </div>
-            <Image src={acmLogo} alt='ACM Logo' width={500} height={500} />
+            <Image src={acmLogo} alt={'ACM Logo'} width={500} height={500} />
             <Footer />
         </div>
     );
